667810b7584aff9e68212cfc4a71c2bfa1768352
hywenklis <hywenklis@hotmail.com>
2022-10-26 21:06:56 -0300

    ajustes de classes

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,14 +1,4 @@
 public class Pessoa {
-<<<<<<< HEAD
-    private String cpf;
-    private String nome;
-=======
-	
-	//---- olá mundo --- 
-	//--- ajeitei
-
 	private String cpf;
 	private String nome;
-
->>>>>>> parent of dff0cb1... ajustes
 }
