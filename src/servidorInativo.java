41175db73db6d3ce32e166459145c3d145e85091
Aysiane <aysianesarah@gmail.com>
2022-11-01 11:38:23 -0300

    ajustes de classes

---
+++ src/servidorInativo.java
@@ -0,0 +1,15 @@
+
+public class servidorInativo extends Pessoa {
+	
+	private String vinculo;
+	private float total;
+	private float liquido;
+	private float proventos;
+	private float eventuais; 
+	private float judiciais;
+	private float tetoRedutor;
+	private float irrf;
+	private float contribuicao;
+	private float previdencia;
+
+}
