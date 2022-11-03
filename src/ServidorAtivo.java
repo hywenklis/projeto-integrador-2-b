9f129bd179b8e533343fed379d1f1c0e04a89acd
hywenklis <hywenklis@hotmail.com>
2022-11-03 17:21:31 -0300

    adiciona metodos de get e set

--- src/ServidorAtivo.java
+++ src/ServidorAtivo.java
@@ -13,4 +13,100 @@ public class ServidorAtivo extends Pessoa {
     private float contribPrev;
     private float totalLiquido;
     private Orgao orgao;
+
+    public LocalDate getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(LocalDate periodo) {
+        this.periodo = periodo;
+    }
+
+    public float getRemuneracao() {
+        return remuneracao;
+    }
+
+    public void setRemuneracao(float remuneracao) {
+        this.remuneracao = remuneracao;
+    }
+
+    public float getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(float comissao) {
+        this.comissao = comissao;
+    }
+
+    public float getBeneficios() {
+        return beneficios;
+    }
+
+    public void setBeneficios(float beneficios) {
+        this.beneficios = beneficios;
+    }
+
+    public float getEventuais() {
+        return eventuais;
+    }
+
+    public void setEventuais(float eventuais) {
+        this.eventuais = eventuais;
+    }
+
+    public float getHorasExtras() {
+        return horasExtras;
+    }
+
+    public void setHorasExtras(float horasExtras) {
+        this.horasExtras = horasExtras;
+    }
+
+    public float getJudiciais() {
+        return judiciais;
+    }
+
+    public void setJudiciais(float judiciais) {
+        this.judiciais = judiciais;
+    }
+
+    public float getTetoRedutor() {
+        return tetoRedutor;
+    }
+
+    public void setTetoRedutor(float tetoRedutor) {
+        this.tetoRedutor = tetoRedutor;
+    }
+
+    public float getIRRF() {
+        return IRRF;
+    }
+
+    public void setIRRF(float IRRF) {
+        this.IRRF = IRRF;
+    }
+
+    public float getContribPrev() {
+        return contribPrev;
+    }
+
+    public void setContribPrev(float contribPrev) {
+        this.contribPrev = contribPrev;
+    }
+
+    public float getTotalLiquido() {
+        return totalLiquido;
+    }
+
+    public void setTotalLiquido(float totalLiquido) {
+        this.totalLiquido = totalLiquido;
+    }
+
+    public Orgao getOrgao() {
+        return orgao;
+    }
+
+    public void setOrgao(Orgao orgao) {
+        this.orgao = orgao;
+    }
 }
