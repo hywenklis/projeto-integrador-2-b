ebc34fca2540d2567f3ff586b7633718a930360e
Aysiane <aysianesarah@gmail.com>
2022-11-07 22:42:58 -0300

    Removendo teste.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
-import utils.Formatacao;
-
-import java.math.BigDecimal;
-
 import static Dados.ServidoresAtivosDados.dadosServidoresAtivos;
 import static Dados.ServidoresInativosDados.dadosServidorInativos;
 
 public class Main {
     public static void main(String[] args) {
-//        dadosServidoresAtivos();
-//        dadosServidorInativos();
-        System.out.println(Formatacao.formatarEmReais(BigDecimal.ZERO));
-        System.out.println( Formatacao.formatarEmReais(BigDecimal.valueOf(200.99)));
-        System.out.println( Formatacao.formatarEmReais(BigDecimal.valueOf(100)));
-
+        dadosServidoresAtivos();
+        dadosServidorInativos();
     }
 }
